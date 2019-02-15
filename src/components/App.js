92050950680c1f9d319ed741e4948a340c99d1fe
ehosaini <me.hosaini@gmail.com>
2019-02-15 11:45:22 -0500

    Initial project setup

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {  BrowserRouter, Route, Link } from 'react-router-dom';
+
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
+
+const App = (props) => {
+    return (
+        <div> 
+            <Header />
+            <BrowserRouter>
+            <div>
+                <Route path="/" exact component={StreamList} />
+                <Route path="/admin/streams" exact component={StreamShow} />
+                <Route path="/streams/new" exact component={StreamCreate} />
+                <Route path="/streams/edit" exact component={StreamEdit} />
+                <Route path="/streams/show" exact component={StreamShow} />
+                <Route path="/streams/delete" exact component={StreamDelete} />
+            </div>   
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
+
---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StreamCreate = (props) => {
+    return(
+        <div>StreamCreate</div>
+    );
+}
+
+export default StreamCreate;
