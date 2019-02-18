05fabb43e7f040c14d7e22ce4750b97beea7f010
ehosaini <me.hosaini@gmail.com>
2019-02-18 10:48:07 -0500

    Pass additional props to createStream form
    -Add some styling

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,28 +2,32 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput(formProps) {
-        return(
-            // <input 
-            //     value={formProps.input.value}
-            //     onChange={formProps.input.onChange}
-            // />
+    // renderInput(formProps) {
+    //     return(
+    //         // <input // --------------- tie only select input properties
+    //         //     value={formProps.input.value}
+    //         //     onChange={formProps.input.onChange}
+    //         // />
 
-            <input { ...formProps.input } /> //short way of doing the above per docs
-
-            // below is very most concise way to write this method
-        );
-    }
-
-    // renderInput({ input }){
-    //     return <input {...input} />
+    //         // <input { ...formProps.input } /> // ---------------- tie all the input properties
+    //     );
     // }
+
+    renderInput({ input, label }){ // above is a more verbose way of achieving this - note how input can be returned
+        return (
+            <div className="field">
+                <label>{ label }</label>
+                <input {...input} />
+            </div>
+        );
+        
+    }  
 
     render() {
         return(
-            <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput} />
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter title"/>
+                <Field name="description" component={this.renderInput} label="Enter description" />
             </form>
         );
     }
