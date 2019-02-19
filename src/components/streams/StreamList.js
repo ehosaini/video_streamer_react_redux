918e52c0dbe3ba437f0986b8320f4d9d8f6426f6
ehosaini <me.hosaini@gmail.com>
2019-02-19 16:35:36 -0500

    Create reducer for streams and wire up fetchStreams action

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamList = (props) => {
-    return (
-        <div>StreamList</div>
-    );
+import { fetchStreams } from '../../actions';
+
+class StreamList extends React.Component {
+    componentDidMount(){
+        this.props.fetchStreams();
+    }
+
+    render(){
+        return (
+            <div>StreamList</div>
+        );
+    }
+    
 }
 
-export default StreamList;
+export default connect(null, { fetchStreams })(StreamList);
---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,21 @@
+import { CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, UPDATE_STREAM, DELETE_STREAM} from '../actions/types';
+
+export default (state={}, action) => {
+    switch(action.type) {
+        case FETCH_STREAMS:
+            const records = {};
+            action.payload.forEach( record => records[record.id] = record);
+            return {...state, ...records};
+        case CREATE_STREAM:
+            return {...state, [action.payload.id]: action.payload}
+        case FETCH_STREAM:
+            return {...state, [action.payload.id]: action.payload};
+        case UPDATE_STREAM:
+            return {...state, [action.payload.id]: action.payload};
+        case DELETE_STREAM:
+            const newState = state.delete(action.payload) //payload holds the id of action the stream 
+            return newState;
+        default:
+            return state;
+    } 
+};
